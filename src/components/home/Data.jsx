3260a99479f66c732910373c4f73a165caf1b76f
student-ragini <kriragini2728@gmail.com>
2026-06-25 06:10:19 +0530

    Updated portfolio projects

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -6,13 +6,15 @@ const Data = () => {
     <div className='home_data'>
       <h3 className='home_title'>
         Ragini Kumari
+         </h3>
+      
         <svg
           width="36"
           height="36"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          class="home__hand"
+          className="home_hand"
         >
           <path
             d="M25.4995 32.0305L31.3495 33.1555L36.1495 8.48051C36.4495 6.83051 35.3995 5.18051 33.8245 4.88051C32.1745 4.58051 30.5995 5.70551 30.2995 7.35551L25.4995 32.0305Z"
@@ -55,13 +57,15 @@ const Data = () => {
             fill="#EBA352"
           ></path>
         </svg>
-      </h3>
+        
+     
+      
       <h3 className='home_subtitle'>React Developer</h3>
       <p className='home_description'>I'm a React Developer and passionate to my work.</p>
       <p className='button-flex'><a href="#contact" className="button button--flex">
         Say Hello
         <svg
-          class="button__icon"
+          className="button__icon"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
