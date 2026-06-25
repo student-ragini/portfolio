098e30435dc22bdb01278d907feef4d560b2b3db
student-ragini <kriragini2728@gmail.com>
2026-06-25 06:28:27 +0530

    Updated portfolio projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import "./projects.css";
 import CareerHub from './CareerHub'
 import DocBooker from './DocBooker';
+import VideoLibrary from './videolibrary';
+import ShoppingOnline from './shoppingonline';
 
 
 const Projects = () => {
@@ -13,6 +15,8 @@ const Projects = () => {
         <div className='skills_containe container grid' id='skills_container'>
             <CareerHub/>
             <DocBooker />
+            <VideoLibrary/>
+            <ShoppingOnline />
         </div>
     </section>
   )
---
+++ src/components/projects/shoppingonline.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ShoppingOnline = () => {
+  return (
+    <div className="skills_content">
+        <h3 className="skills_title">ShoppingOnline</h3>
+        <div className='docDiv'><img src="/ishop.jpeg" alt="ShoppingOnline" className='docImage' /></div>
+        <div style={{margin:"5px"}}><a href="https://shopping-frontend-lac.vercel.app/" target='_blank' className='demolink'>See Demo <span className='bi bi-arrow-right'></span></a></div>
+    </div>
+  )
+}
+
+export default ShoppingOnline
